a1b4c1891483b2305ae92d87a37cbb5f1b490263
Gabocoelho8 <64917906+Gabocoelho8@users.noreply.github.com>
2023-02-27 14:56:13 -0400

    Add files via upload

---
+++ AFTERGLOW/Afterglow/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (<>
+    <div className="App">
+      <Navbar />
+      <Hero/>
+    </div>
+    </>
+  );
+}
+
+export default App
